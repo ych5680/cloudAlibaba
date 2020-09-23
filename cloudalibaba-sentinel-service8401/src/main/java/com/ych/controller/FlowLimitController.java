d0db6c4089e6e45d4d270c6375654f95d4dc50dd
yuz <568015364@qq.com>
2020-09-23 15:55:36 +0800

    增加代码

--- cloudalibaba-sentinel-service8401/src/main/java/com/ych/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/ych/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.ych.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,6 +24,20 @@ public class FlowLimitController {
 
         return "------testB";
     }
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        int age = 10/0;
+        return "------testHotKey";
+    }
+
+    //兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
+    }
+
+
 
 
 }
